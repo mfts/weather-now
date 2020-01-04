c9a4dabb920ef87b6336f3b27d98b7894acc553f
Marc Seitz <marc@mfts.io>
2020-01-04 16:47:26 +0200

    add accuweather current conditions api endpoint

--- server/index.js
+++ server/index.js
@@ -43,4 +43,14 @@ app.get('/api', async function (req, res) {
   res.status(200).send(data);
 });
 
+app.post('/api/accuweather', async function (req, res) {
+  console.log(req.body)
+  const secret = process.env.ACCUWEATHER_API_KEY;
+  const baseUrl = 'http://dataservice.accuweather.com/currentconditions/v1';
+  const endpoint = (locationKey, detailsYes) => `${baseUrl}/${locationKey}?apikey=${secret}&details=${detailsYes}`;
+  const data = await getWeatherCondition(endpoint(req.body.locationKey, req.body.detailsYes));
+
+  res.status(200).send(data)
+})
+
 const server = app.listen(PORT, () => console.log(`app running on port: ${server.address().port}`));
